4a03eff5321f6c31a1b501a434608a56c44109c8
charles <cle4@njit.edu>
2022-02-08 10:40:23 -0500

    name change

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Charles Espulgar"
+        name = "Charles espulgar"
         return render_template('index.html', name=name)
